3960fb50482d74407d98603d86a24540c157eafd
Gonzalo Birrueta <silentcr7@gmail.com>
2019-09-23 06:28:30 -0700

    refactored classes

--- dog.py
+++ dog.py
@@ -5,9 +5,3 @@ class Dog:
 
     def bark(self):
         print("Woof!")
-
-my_dog = Dog("Paris","German Shepard")
-
-print(my_dog)
-print(my_dog.name, my_dog.breed)
-my_dog.bark()
---
+++ my_dogs.py
@@ -0,0 +1,7 @@
+import dog
+
+my_dog = Dog("Paris","German Shepard")
+
+print(my_dog)
+print(my_dog.name, my_dog.breed)
+my_dog.bark()
