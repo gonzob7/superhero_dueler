56f38f384cf6e6ce99c65bc81e3787ac5a0ce84c
Gonzalo Birrueta <silentcr7@gmail.com>
2019-09-23 06:53:19 -0700

    created attack ability with random strength

--- superheroes.py
+++ superheroes.py
@@ -1,8 +1,19 @@
+import random
+
 class Ability:
-    def __init__(self, name, attack_strength):
+    def __init__(self, name, max_damage):
         '''Create Instance Variables:
           name:String
           max_damage: Integer
         '''
         self.name = name
-        self.attack_strength = max_damage
+        self.max_damage = max_damage
+
+    def attack(self):
+        strength = random.randint(0, self.max_damage)
+        return strength
+
+if __name__ == "__main__":
+    ability = Ability("Debugging Ability", 20)
+    print(ability.name)
+    print(ability.attack())
