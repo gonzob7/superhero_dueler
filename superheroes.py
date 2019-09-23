a701c56c2b6d6a124025707a138144474cf92b2e
Gonzalo Birrueta <silentcr7@gmail.com>
2019-09-23 06:46:30 -0700

    created ability class, with parameters name/attack_strength

---
+++ superheroes.py
@@ -0,0 +1,8 @@
+class Ability:
+    def __init__(self, name, attack_strength):
+        '''Create Instance Variables:
+          name:String
+          max_damage: Integer
+        '''
+        self.name = name
+        self.attack_strength = max_damage
