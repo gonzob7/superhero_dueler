6acaf01238203b4fd642f5560b8e11075e28547d
Gonzalo Birrueta <silentcr7@gmail.com>
2019-09-23 06:20:42 -0700

    added breed on init

--- dog.py
+++ dog.py
@@ -1,8 +1,10 @@
 class Dog:
-    def __init__(self, name):
+    def __init__(self, name, breed):
         self.name = name
+        self.breed = breed
 
-my_dog = Dog("Paris")
+my_dog = Dog("Paris","German Shepard")
 
 print(my_dog)
 print(my_dog.name)
+print(my_dog.breed)
